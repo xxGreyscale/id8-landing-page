3f16de9f4608d9b48f65e62a1b02652c087fe3af
salim <salim.nassor.sn@gmail.com>
2021-08-04 17:28:45 +0300

    style(navigation bar): improved navigation bar styling

--- src/app/components/header/navigation.component.ts
+++ src/app/components/header/navigation.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
 
+  yPosition: any = 0;
+
+  @HostListener('window:scroll', [`$event`]) onWindowScroll(): void {
+    // console.log(window.scrollY);
+    this.yPosition = window.scrollY;
+ }
+
+
+  isCollapsed: any = false;
+
+  onHambugerClick = () => {
+    this.isCollapsed = !this.isCollapsed
+  }
+
   constructor() { }
 
+  getYPosition(e: Event): number {
+    return (e.target as Element).scrollTop;
+  }
+
   ngOnInit(): void {
+    
   }
 
 }
