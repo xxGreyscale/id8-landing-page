df94e8504def8fbbb80efd3ad5cfe78e6e89c21b
salim <salim.nassor.sn@gmail.com>
2021-08-05 15:30:56 +0300

    feat(...): added about page in pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
+    AboutPageComponent,
   ],
   imports: [
     CommonModule,
