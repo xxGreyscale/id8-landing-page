f922699ceba605483bb6f6719adff885370f547e
salim <salim.nassor.sn@gmail.com>
2021-08-05 15:40:24 +0300

    feat(pages-routing module): added route to about us page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
+  },
+  {
+    path: 'about-us',
+    component: AboutPageComponent
   }
 ];
 
