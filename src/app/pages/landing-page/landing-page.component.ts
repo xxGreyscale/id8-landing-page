260be5c5895d5bc1e8f3ce5b4e513b6e42ed9bec
salim <salim.nassor.sn@gmail.com>
2021-08-14 22:39:51 +0300

    style(...): added animation to the main title

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChange } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  titles: string[] = [
+    `<span class="changing-text"> Create. </span>`,
+    `<span class="changing-text"> Connect. </span>`,
+    `<span class="changing-text"> Collaborate. </span>`
+  ]
 
+  currentIndex = 0;
+  currentTitle = this.titles[this.currentIndex];
+  heading: any;
+
+
+  // function
+  changeTitle = () => {
+    this.currentIndex = (this.currentIndex + 1)%this.titles.length
+    setTimeout(this.changeTitle, 2000);
+  }
+
+  ngDoCheck() {
+    if (this.currentIndex >= 0) {
+      this.heading = this.titles[this.currentIndex]
+    }
+    
+  }
+
+  constructor() {
+    this.changeTitle();
+  }
+  
   ngOnInit(): void {
+    this.heading = new BehaviorSubject(this.titles[this.currentIndex]);
   }
 
 }
