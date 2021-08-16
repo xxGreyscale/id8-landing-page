2ca0bb73774557cf268a86a50dd757de56ce2c1a
salim <salim.nassor.sn@gmail.com>
2021-08-16 10:07:45 +0300

    feat(...): added anonymous pro monospace font

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -8,55 +8,79 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class LandingPageComponent implements OnInit {
 
+  carousels: any[] = [
+    {
+      content: 
+      `Take you innovative business idea to the next level and join tailored programs
+       that include access to exclusive development support dedicated workshops and mentors`,
+      image: 'assets/img/kid.png'
+
+    },
+    {
+      content: 
+      `A space connecting people & ideas to business resources;
+       promoting collaboration and sharing of collective knowledge to support your journey.`,
+      image: 'assets/img/kid.png'
+
+    },
+    {
+      content: 
+      `A space connecting people & ideas to business resources;
+       promoting collaboration and sharing of collective knowledge to support your journey.`,
+      image: 'assets/img/kid.png'
+
+    }
+  ]
+
+
   titles: string[] = [
     `<span class="changing-text"> Create. </span>`,
     `<span class="changing-text"> Connect. </span>`,
     `<span class="changing-text"> Collaborate. </span>`
   ]
 
-  currentIndex = 0;
-  currentTitle = this.titles[this.currentIndex];
+  titleTimeout = 3000
+  carouselTimeout = this.titleTimeout*4
+
+  currentTitleIndex = 0;
+  currentCarouselIndex = 0;
+  currentTitle = this.titles[this.currentTitleIndex];
   heading: any;
 
-  carousel: any;
-  carouselContent: any; 
+  currentCarousel: any; 
 
 
   // function
   changeTitle = () => {
-    this.currentIndex = (this.currentIndex + 1)%this.titles.length
-    setTimeout(this.changeTitle, 2000);
+    this.currentTitleIndex = (this.currentTitleIndex + 1)%this.titles.length
+    setTimeout(this.changeTitle, this.titleTimeout);
   }
 
+  changeCarouselContents = () => {
+    this.currentCarouselIndex = (this.currentCarouselIndex + 1)%this.carousels.length
+    setTimeout(this.changeCarouselContents, this.carouselTimeout);
+  }
+  
   ngDoCheck() {
-    if (this.currentIndex >= 0) {
-      this.heading = this.titles[this.currentIndex]
+    if (this.currentTitleIndex >= 0) {
+      this.heading = this.titles[this.currentTitleIndex]
+    }
+
+    if(this.currentCarouselIndex >= 0) {
+      this.currentCarousel = this.carousels[this.currentCarouselIndex]
     }
     
   }
 
   constructor() {
     this.changeTitle();
+    this.changeCarouselContents()
   }
   
   ngOnInit(): void {
-    this.heading = new BehaviorSubject(this.titles[this.currentIndex]);
-    this.carousel = [
-      {
-        content: 
-        `Take you innovative business idea to the next level and join tailored programs
-         that include access to exclusive development support dedicated workshops and mentors`,
-        image: 'assets/img/kid-light.png'
-
-      },
-      {
-        content: 
-        `A space connecting people & ideas to business resources;
-         promoting collaboration and sharing of collective knowledge to support your journey.`,
-        image: 'assets/img/kid-light.png'
-
-      }
-    ]
+   
+    // this.heading = new BehaviorSubject(this.titles[this.currentTitleIndex]);
+    // this.currentCarousel = new BehaviorSubject(this.carousels[this.currentCarouselIndex])
   }
 
 }
