1428f25151dae716bc02d7d7fa5f9ad4ca768d65
salim <salim.nassor.sn@gmail.com>
2021-08-20 16:36:07 +0300

    feat(...): added rss

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { XmlParser } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from 'src/app/services/news/news.service';
 import { UserService } from 'src/app/services/user/user.service';
+import * as xml2js from 'xml2js';
+
 
 @Component({
   selector: 'app-about-page',
@@ -11,11 +15,14 @@ export class AboutPageComponent implements OnInit {
 
   user: any;
   news: any[] = [];
-  constructor(private newService: NewsService, private userService: UserService) { }
+  xmlItems: any;
+  
+  constructor(private newService: NewsService, private userService: UserService, private _http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getNews()
-    this.userService.getHeader()
+    // this.getNews()
+    // this.userService.getHeader()
+    this.getRssXML()
   }
   
   getNews() {
@@ -24,6 +31,50 @@ export class AboutPageComponent implements OnInit {
       this.news = response['data'];
       this.news = this.news.slice(0,4)
     })
+  }
+
+  getRssXML() {
+    this._http.get('https://rss.app/feeds/seNz2zzMDf1JD9Cv.xml',  
+      {  
+        headers: new HttpHeaders()  
+          .set('Content-Type', 'text/xml')  
+          .append('Access-Control-Allow-Methods', 'GET')  
+          .append('Access-Control-Allow-Origin', '*')  
+          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),  
+        responseType: 'text'  
+      })  
+      .subscribe((data) => {  
+        this.parseXML(data)  
+          .then((data) => {  
+            this.xmlItems = data;  
+          });  
+      });  
+  }
+
+  parseXML(data: any) {  
+    return new Promise(resolve => {  
+      var k: string | number,  
+        arr: any[] = [],  
+
+        parser = new xml2js.Parser(  
+          {  
+            trim: true,  
+            explicitArray: true  
+          });  
+      parser.parseString(data, function (err: any, result: any) {  
+        var obj = result.Employee;  
+        for (k in obj.emp) {  
+          var item = obj.emp[k];  
+          arr.push({  
+            id: item.id[0],  
+            name: item.name[0],  
+            gender: item.gender[0],  
+            mobile: item.mobile[0]  
+          });  
+        }  
+        resolve(arr);  
+      });  
+    });  
   }
 
 }
