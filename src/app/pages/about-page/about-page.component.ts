1aa85d212f4df7644dbfdb1689d2ff27faf7b3fe
salim <salim.nassor.sn@gmail.com>
2021-09-22 16:46:41 +0300

    feat(about page): added seo to about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, Meta, Title } from '@angular/platform-browser';
 import { NewsService } from 'src/app/services/news/news.service';
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -17,72 +17,20 @@ var parser = require('fast-xml-parser');
 })
 export class AboutPageComponent implements OnInit {
 
-  user: any;
-  news: any[] = [];
-  newsWithImg: any[] = [];
-  loading: Boolean = false;
+  title = 'About Id8 Space'
 
-  urls: String[] = [
-    'https://gene-aapi.herokuapp.com/https://disrupt-africa.com/category/region/east-africa/feed/',
-    'https://gene-aapi.herokuapp.com/https://disrupt-africa.com/feed/',
-    `https://gene-aapi.herokuapp.com/`
-  ]
-  
-  currentURL: String = this.urls[0];
-  currentURLIndex: number = 0;
-
-
-  constructor(private newService: NewsService, private _http: HttpClient) {
-    // this.getNews()    
+  constructor(private titleService: Title, 
+              private metaService: Meta) {
    }
 
   ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'hub, space, tanzania, 255, id8 space, id8, Dar es salaam, east africe, tech space, tech, technology'},
+      {name: 'description', content: 'A space connecting people & ideas to business resources; promoting collaboration and knowledge-sharing to support your journey.Angular Universal Example'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
   }
 
-
-  changeCurrentFeed(index: number) {
-    this.currentURLIndex = index;
-    this.currentURL = this.urls[index]    
-    this.getNews()    
-  }
-
-  fetchMeta(news: any[]) {
-    const domParser = new DOMParser()
-    this.newsWithImg = []
-    let HTTPOptions:Object = {
-
-      headers: new HttpHeaders({
-      }),
-      responseType: 'text'
-   }
-
-    news.forEach(element => {
-      this._http.get(`https://gene-aapi.herokuapp.com/${element.guid}`, HTTPOptions).subscribe(response => {
-      let  responseString: any = domParser.parseFromString(response.toString(), 'text/html')
-      let metaImgs: any[] = responseString.head.querySelectorAll("meta[property='og:image']")
-      metaImgs.forEach(tag => {
-        let tmpImg = tag.getAttribute("content");
-        let newsWithImage = {
-          news: element,
-          image: tmpImg
-        }
-        this.newsWithImg.push(newsWithImage)
-        this.loading = false;
-      });
-    })
-  })
-}
-  
-  getNews() {    
-    this.loading = true;
-    let tmp = this.newService.getNewsFromServer(this.currentURL)
-    tmp.subscribe(response => {
-      let xmlParsedText = parser.parse(response);
-      this.news = xmlParsedText.rss.channel.item;
-      this.news = this.news.slice(0,4)
-      
-      this.fetchMeta(this.news)
-    })
-  }
 
 }
